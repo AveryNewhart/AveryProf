82e1be5319d2341aa8489e5945e3d45ce61fa425
AveryNewhart <newhartreeceavery@gmail.com>
2024-10-02 18:01:05 -0400

    change made to router file to have other pages work

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,26 +1,25 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ProjectPage from "./pages/ProjectPage";
-import ContactPage from './pages/ContactPage'
+import ContactPage from './pages/ContactPage';
 import ResumePage from "./pages/ResumePage";
 
-//! COLORS PALLETE FOR THE PORT!!!!!
+//! COLORS PALETTE FOR THE PORT!!!!!
 // https://coolors.co/palette/1a1b1b-262726-33442c-bcabae-ddd5d0
 
-
-export default function Router() {
+export default function AppRouter() {
     return (
       <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/project" element={<ProjectPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/resume" element={<ResumePage />} />
-        </Routes>
-       </BrowserRouter>
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/project" element={<ProjectPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/resume" element={<ResumePage />} />
+          </Routes>
+        </Router>
       </>
     );
-  }
+}
