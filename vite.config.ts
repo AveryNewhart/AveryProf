e59e2e191707ef6752aa929ab276781059839d7d
AveryNewhart <newhartreeceavery@gmail.com>
2024-10-07 17:03:39 -0400

    troubleshooting

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  },
-  server: {
-    fs: {
-      strict: false,
-    },
-  },
 })
